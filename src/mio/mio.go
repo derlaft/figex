94a55c3733d3a8e3ec2ba0b76bfd3624f8c2a775
Alexey Derlaft <derlafff@ya.ru>
2015-03-24 22:01:12 +0300

    got rid of useless mmap

--- src/mio/mio.go
+++ src/mio/mio.go
@@ -2,59 +2,35 @@ package mio
 
 import (
     "os"
-    "github.com/edsrzf/mmap-go"
+    "bufio"
 )
 
 type Prog struct {
-    file *os.File
-    mmap mmap.MMap
     Str []string
 }
 
 const MAXLEN = 2048
 
-func (p *Prog) makedb() {
-
+func Mmap(path string) (p Prog, err error) {
     p.Str = make([]string, 512, MAXLEN)
 
-    var f, n int
-
-    for c := 1; c < len(p.mmap) && c < MAXLEN; c += 1 {
-        if p.mmap[c] == '\n' {
-            p.Str[n] = string(p.mmap[f:c])
-            n += 1
-            c += 1
-            f = c
-        }
-    }
-
-
-
-}
-
-func Mmap(path string) (p Prog, err error) {
-
-    p.file, err = os.Open(path)
+    file, err := os.Open(path)
 
     if err != nil {
         return p, err
     }
+    defer file.Close()
 
-    p.mmap, err = mmap.Map(p.file, mmap.RDONLY, 0)
+    scanner := bufio.NewScanner(file)
 
-    if err != nil {
-        p.Close()
-        return p, err
+    n := 0
+    for ; scanner.Scan(); n++ {
+            p.Str[n] = scanner.Text()
     }
 
-    p.makedb()
+    //cut slice so probably GC will free some space
+    p.Str = p.Str[:n]
+
     return p, nil
 }
-
-func (p *Prog) Close() {
-    p.Str = nil
-    p.mmap.Unmap()
-    p.file.Close()
-}
-
 
--- src/mio/mio_test.go
+++ src/mio/mio_test.go
@@ -7,20 +7,19 @@ import (
 
 func TestOpen(t *testing.T) {
 
-    mmap, err := Mmap("./test.txt")
+    prog, err := Mmap("./test.txt")
     if err != nil {
         fmt.Println(err)
         t.Fail()
     }
 
-    if  mmap.Str[0] != "TEST1" ||
-        mmap.Str[1] != "TEST2" ||
-        mmap.Str[2] != "MEOWOWOWOWWOWOOW?" ||
-        mmap.Str[3] != "PUR PUR ;MAGIC" ||
-        mmap.Str[4] != "Nyan)" {
+    if  prog.Str[0] != "TEST1" ||
+        prog.Str[1] != "TEST2" ||
+        prog.Str[2] != "MEOWOWOWOWWOWOOW?" ||
+        prog.Str[3] != "PUR PUR ;MAGIC" ||
+        prog.Str[4] != "Nyan)" {
             t.Fail()
         }
 
-    defer mmap.Close()
 }
 
